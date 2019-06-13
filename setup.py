83838b2919faa238006755c43c23ed99771ed69d
blaze <blaze@vivaldi.net>
2019-06-13 18:01:31 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tvnao',
-    version='0.10.0',
+    version='0.10.97',
     description='tv here and now',
     long_description='a tool for watching IP TV, shows TV guide',
     author='blaze',
